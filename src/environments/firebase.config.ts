5a755770784c557c228687ffdd5a7d3936b197b5
marcedem <marcsea2000@gmail.com>
2017-07-13 18:35:49 +0200

    polulating firebase real time database

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,12 +1,8 @@
-
-
 export const firebaseConfig = {
-    apiKey: "AIzaSyA0BcUcu4V8aHT_gM-32BhRcmqji4z-lts",
-    authDomain: "final-project-recording.firebaseapp.com",
-    databaseURL: "https://final-project-recording.firebaseio.com",
-    storageBucket: "final-project-recording.appspot.com",
-    messagingSenderId: "290354329688"
+    apiKey: "AIzaSyAFG2bWg0rXGY6bz20U9DIo_zck1oKJomw",
+    authDomain: "angularelearningplatform.firebaseapp.com",
+    databaseURL: "https://angularelearningplatform.firebaseio.com",
+    storageBucket: "angularelearningplatform.appspot.com",
+    messagingSenderId: "898399226903",
+    projectId: "angularelearningplatform"
 };
-
-
-
