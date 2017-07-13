b56a75e78485abb408dfbb4cc08be6a46825c648
marcedem <marcsea2000@gmail.com>
2017-07-13 18:04:21 +0200

    firebase integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { initializeApp, database } from 'firebase';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+	constructor(){
+		// Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyAFG2bWg0rXGY6bz20U9DIo_zck1oKJomw",
+            authDomain: "angularelearningplatform.firebaseapp.com",
+            databaseURL: "https://angularelearningplatform.firebaseio.com",
+            projectId: "angularelearningplatform",
+            storageBucket: "",
+            messagingSenderId: "898399226903"
+        };
+        initializeApp(config);
+        var root = database().ref('messages/2');
+
+        root.on('value', function(snap){
+            console.log(snap.key, snap.val());
+        });
+	}
+  
+
+  	title = 'app works!';
 }
